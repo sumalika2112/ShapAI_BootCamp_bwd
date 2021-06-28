a6005f417fe268381b2425a51b3caddf193e330b
sumalika2112 <86605084+sumalika2112@users.noreply.github.com>
2021-06-28 17:30:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info() {
+   return (
+    <div className="note">
+    <h1>Javascript and react.js</h1>
+    <p>A basic web dev React JS bootcamp</p>
+    </div>
+    );
+}
+
+export default Info;
